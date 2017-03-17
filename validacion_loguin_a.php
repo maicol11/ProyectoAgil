1c28b7cc7dc2008516ae871508e7c773eb846656
Maicol Stiven <maicol11ce@live.com>
2017-03-17 12:40:52 -0500

    Sub Catalogo Ubisoft 2.0

--- validacion_loguin_a.php
+++ validacion_loguin_a.php
@@ -1,11 +1,11 @@
 <?php 
 			$correo = $_POST['correo'];
 			$contrasena = $_POST['contrasena'];
- 			$sql = "SELECT nombre, apellido FROM administrador WHERE correo = '$correo' AND contrasena = '$contrasena'";
+ 			$sql = "SELECT nombre FROM administrador WHERE correo = '$correo' AND contrasena = '$contrasena'";
 			$query = mysqli_query($con, $sql);
  			if(mysqli_num_rows($query) > 0){
  				$row = mysqli_fetch_array($query);
- 				$_SESSION['nombre'] = $row['nombre']." ".$row['apellido']; 		
+ 				$_SESSION['nombre'] = $row['nombre']; 		
  				header("location: dashboardadm.php");		
  			}else {
  				echo "<script>
